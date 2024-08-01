195c366a909b97a2b65f1cb01b6df56247b0b530
Ilya <@iIlyaM>
2024-08-01 21:43:48 +0300

    Add ``TicketsDto.java``

---
+++ src/main/java/ru/ilyam/dto/TicketsDto.java
@@ -0,0 +1,16 @@
+package ru.ilyam.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketsDto {
+    @JsonProperty("tickets")
+    private List<TicketDto> tickets;
+}
